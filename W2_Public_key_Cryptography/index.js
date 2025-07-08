5ecf73c4c8ef472e1e48a8554f7a06e5be62f782
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2025-07-08 18:21:35 +0530

    UInt8Array2ascii and vice versa, bytes2ascii and vice versa

---
+++ W2_Public_key_Cryptography/index.js
@@ -0,0 +1,56 @@
+// UInt8Array to ascii
+
+// function bytesToAscii(byteArray) {
+//   return new TextDecoder().decode(byteArray);
+// }
+// // Example usage:
+// const bytes = new Uint8Array([72, 101, 108, 108, 111]); // Corresponds to "Hello"
+// const asciiString = bytesToAscii(bytes);
+// console.log(asciiString); // Output: "Hello"
+
+
+// Ascii to UInt8Array
+
+// function asciiToBytes(asciiString) {
+//     const arr = [];
+//     for (let i = 0; i < asciiString.length; i++) {
+//         const char = asciiString[i];
+//         arr.push(char.charCodeAt(0));
+//     }
+//     return new Uint8Array(arr);
+//     // 2nd method using TextEncoder
+//     // return new Uint8Array([...asciiString].map(char => char.charCodeAt(0)));
+// }
+
+// // Example usage:
+// const ascii = "Hello";
+// const byteArray = asciiToBytes(ascii);
+// console.log(byteArray); // Output: Uint8Array(5) [72, 101, 108, 108, 111]
+
+
+// Bytes to Ascii
+
+// function bytesToAscii(byteArray) {
+//   return byteArray.map(byte => String.fromCharCode(byte)).join('');
+// }
+
+// // Example usage:
+// const bytes = [72, 101, 108, 108, 111]; // Corresponds to "Hello"
+// const asciiString = bytesToAscii(bytes);
+// console.log(asciiString); // Output: "Hello"
+
+
+// Ascii to bytes
+
+// function asciiToBytes(asciiString) {
+//   const byteArray = [];
+//   for (let i = 0; i < asciiString.length; i++) {
+//     byteArray.push(asciiString.charCodeAt(i));
+//   }
+//   return byteArray;
+// }
+
+// // Example usage:
+// const ascii = "Hello";
+// const byteArray = asciiToBytes(ascii);
+// console.log(byteArray); // Output: [72, 101, 108, 108, 111]
