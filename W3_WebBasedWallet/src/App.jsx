f6052b991466c1a4b7db096631aef13f59335524
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2025-07-14 11:07:21 +0530

    complete web based wallet

--- W3_WebBasedWallet/src/App.jsx
+++ W3_WebBasedWallet/src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { generateMnemonic } from "bip39";
+import { SolanaWallet } from './SolanaWallet';
+import { EthWallet } from './EthWallet';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mnemonic, setMnemonic] = useState("");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Current count is: {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+            type="text"
+            value={mnemonic}
+            onChange={(e) => setMnemonic(e.target.value)} 
+      />
+
+      <button onClick={async function() {
+        const mn = generateMnemonic();
+        setMnemonic(mn)
+      }}>
+        Create Seed Phrase
+      </button>
+
+      {mnemonic && <SolanaWallet mnemonic={mnemonic} />}
+      {mnemonic && <EthWallet mnemonic={mnemonic} />}
     </>
   )
 }
